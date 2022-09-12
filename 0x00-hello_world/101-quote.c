212e21c1f7497f9ec156c5f857cba87f1ad552c0
“AUGUSTINE <blewudziaustine@gmail.com>
2022-09-12 08:05:25 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main entry
+ * main - Entry point
  *
- * Returns 1 Always
+ * Return: Always 1
  */
-int main (void)
+int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
