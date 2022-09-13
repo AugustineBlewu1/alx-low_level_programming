c2b79e4ad3389445d49d5164e45990cf7d48aa9b
“AUGUSTINE <blewudziaustine@gmail.com>
2022-09-12 20:31:19 -0700

    trailing white

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -7,15 +6,13 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
- 	{
+	{
 	printf("%i is positive\n", n);
 	}
 	else if (n == 0)
@@ -27,3 +24,4 @@ int main(void)
 	printf("%i is negative\n", n);
 	}
 	return (0);
+}
